53e7512f3c4cf2c499654dd0b594298f64283ea7
anurag mishra <anurag171@gmail.com>
2021-12-05 20:38:17 +0530

    Added the latest

--- src/main/java/com/taskVersion/taskmanager/configuration/TaskManagerConfig.java
+++ src/main/java/com/taskVersion/taskmanager/configuration/TaskManagerConfig.java
@@ -1,6 +1,7 @@
 package com.taskVersion.taskmanager.configuration;
 
 import com.faunadb.client.FaunaClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 @Configuration
+@Slf4j
 public class TaskManagerConfig{
 
     @Value("${fauna-db.secret}")
@@ -15,10 +17,11 @@ public class TaskManagerConfig{
     @Bean
     @Scope(value = ConfigurableBeanFactory.SCOPE_SINGLETON)
     public FaunaClient faunaConfiguration() {
+        log.info("Inside fauna client");
         FaunaClient faunaClient = FaunaClient.builder()
                 .withSecret(serverKey)
                 .build();
-
+        log.info("returning fauna client");
         return faunaClient;
     }
 }
